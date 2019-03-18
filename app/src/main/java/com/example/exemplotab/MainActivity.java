33dec8edb13eda3413f1379733d9fba28b2aac53
eduardo.oliveira <eduardojo@gmail.com>
2019-03-18 14:37:12 -0300

    Removeu FragmetMain criado por padrão

--- app/src/main/java/com/example/exemplotab/MainActivity.java
+++ app/src/main/java/com/example/exemplotab/MainActivity.java
@@ -104,14 +104,6 @@ public class MainActivity extends AppCompatActivity {
             return fragment;
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
     }
 
     /**
